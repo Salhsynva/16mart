1a91bea32669d47ae3efcf2bcb8c5bfaffac2084
Salhsynva <100703384+Salhsynva@users.noreply.github.com>
2022-03-16 11:02:14 +0300

    Add files via upload

---
+++ 16mart/Program.cs
@@ -0,0 +1,170 @@
+﻿using System;
+
+namespace _16mart
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region task1
+            //int count = 0;
+            //string[] array = new string[2];
+            //Console.WriteLine("arrayin elementlerini daxil edin: ");
+            //for (int i = 0; i < array.Length; i++)
+            //{
+            //    array[i] = Console.ReadLine();
+            //}
+            //foreach (var item in array)
+            //{
+            //    for (int i = 0; i < item.Length; i++)
+            //    {
+            //        switch (item[i])
+            //        {
+            //            case '0':
+            //                count++;
+            //                break;
+            //            case '1':
+            //                count++;
+            //                break;
+            //            case '2':
+            //                count++;
+            //                break;
+            //            case '3':
+            //                count++;
+            //                break;
+            //            case '4':
+            //               count++;
+            //               break;
+            //            case '5':
+            //                count++;
+            //                break;
+            //            case '6':
+            //                count++;
+            //                break;
+            //            case '7':
+            //                count++;
+            //                break;
+            //            case '8':
+            //                count++;
+            //                break;
+            //            case '9':
+            //                count++;
+            //                break;
+            //            default:
+            //                break;
+            //        }
+            //    }
+            //}
+            //if(count>0)
+            //    Console.WriteLine("sozlerde reqem var");
+            //else
+            //    Console.WriteLine("sozlerde reqem yoxdur");
+            #endregion
+
+            #region task2
+            //console.writeline("arrayin elementlerini daxil edin: ");
+            //string[] words = new string[4];
+            //int count = 0; 
+            //for (int i = 0; i < words.length; i++)
+            //{
+            //    words[i] = console.readline();
+            //}      
+            //foreach (var item in words)
+            //{
+            //    for (int j = 0; j < item.length-1; j++)
+            //    {
+            //        if(item[j] == 'a' && item[j+1] == 'l')
+            //        {
+            //            count++;
+            //        }
+            //    }
+            //}
+            //if(count > 0)
+            //    console.writeline("vardir");
+
+            #endregion
+
+            #region task3
+            //string numberStr = Console.ReadLine();
+            //int number = Convert.ToInt32(numberStr);
+            //int sum = 0;
+            //int count = 0;
+            //if(number > 0)
+            //{
+            //   while(number > 0)
+            //   {
+            //        sum += number % 10;
+            //        number = (number - number % 10) / 10;
+            //        count++;
+            //   }
+            //Console.WriteLine(sum/count);
+            //}
+            #endregion
+
+            #region task4
+            //int counter = 0;
+            //string[] array = new string[3];
+            //Console.WriteLine("arrayin elementlerini daxil edin: ");
+            //for (int i = 0; i < array.Length; i++)
+            //{
+            //    array[i] = Console.ReadLine();
+            //}
+            //foreach (var item in array)
+            //{
+            //    int count = 0;
+            //    for (int i = 0; i < item.Length; i++)
+            //    {
+            //        switch (item[i])
+            //        {
+            //            case '0':
+            //                count++;
+            //                break;
+            //            case '1':
+            //                count++;
+            //                break;
+            //            case '2':
+            //                count++;
+            //                break;
+            //            case '3':
+            //                count++;
+            //                break;
+            //            case '4':
+            //                count++;
+            //                break;
+            //            case '5':
+            //                count++;
+            //                break;
+            //            case '6':
+            //                count++;
+            //                break;
+            //            case '7':
+            //                count++;
+            //                break;
+            //            case '8':
+            //                count++;
+            //                break;
+            //            case '9':
+            //                count++;
+            //                break;
+            //            default:
+            //                break;
+            //        }
+            //    }
+            //    if (count > 0) 
+            //    {
+            //        counter++;
+            //    }
+            //}
+            //if (counter > 0)
+            //    Console.WriteLine($"{counter} sozde reqem var");
+            //else
+            //    Console.WriteLine("sozlerde reqem yoxdur");
+            #endregion
+
+
+
+
+
+        }
+    }
+}
